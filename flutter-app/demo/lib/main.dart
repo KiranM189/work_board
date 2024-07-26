4e164de5ed4d33631cfda939821903729ebb1197
KiranM189 <kirankirankiran189@gmail.com>
2024-07-26 21:32:12 +0530

    commit before pulling changes

--- flutter-app/demo/lib/main.dart
+++ flutter-app/demo/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:demo/default_page.dart';
 import 'package:flutter/material.dart';
+// import 'package:demo/on_board_page.dart';
 
 void main(){
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(useMaterial3: true, colorScheme: const ColorScheme.dark(brightness: Brightness.dark)),
-      home: const DefaultPage(),
+      home: DefaultPage(),
       debugShowCheckedModeBanner: false,
     );
   }
